7cfddc35868c96627c5d01e0004d57b0266b8893
micaelmbagira <micael.mbagira@icloud.com>
2015-04-23 10:56:05 +0200

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423084301) do
+ActiveRecord::Schema.define(version: 20150423085121) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20150423084301) do
     t.integer  "key"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "channel"
   end
 
   create_table "channels", force: true do |t|
