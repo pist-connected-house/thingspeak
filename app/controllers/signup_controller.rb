1396629981e0dc3985c0f87680c4857228788ece
Sonia <sonia.pigot@live.fr>
2015-04-23 10:24:35 +0200

    signup

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,5 +1,10 @@
-class SignupController < ApplicationController
+class SignupController < Devise::RegistrationsController
 	layout 'login'
+	
+	# use defaults from devise
+	def new; super; end
+	def edit; super; end
+	def create; super; end
 
 	def signup
 		render 'signup'
