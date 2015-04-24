041f9e66adcce9619424258d7650f0c8b9430272
Sonia <sonia.pigot@live.fr>
2015-04-24 09:21:24 +0200

    controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,9 +44,6 @@ class ApplicationController < ActionController::Base
     params[:sum] = '1440' if params[:sum] == 'daily'
   end
   
-	def after_sign_up_path_for(resource)
-		return '/key_registrations'
-	end
 
   def after_sign_out_path_for(resource)
     return '/appli/login'
--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -7,6 +7,9 @@ class SignupController < Devise::RegistrationsController
 	def create; super; end
 	
 	
+	def after_sign_up_path_for(resource)
+		return '/key_registrations'
+	end
 
 
 end
