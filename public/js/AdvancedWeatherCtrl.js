fcd5c8ae0bd4e9e8e8655766a214d2a586a13a79
micaelmbagira <micael.mbagira@icloud.com>
2015-04-30 23:29:10 +0200

    Test git atom

--- public/js/AdvancedWeatherCtrl.js
+++ public/js/AdvancedWeatherCtrl.js
@@ -1,6 +1,6 @@
 pistApp.controller('AdvancedWeatherController', ['$scope', '$http', "$interval", function($scope, $http, $interval){
 	$scope.tempercu;
-	
+
     $scope.control = function(){
         $http.get('http://kgb.emn.fr:8001/channels/5/feed.json?key=ZSAVTBI11WQOSJWY&results=1')
 	.then(function(result) {
@@ -10,23 +10,24 @@ pistApp.controller('AdvancedWeatherController', ['$scope', '$http', "$interval",
 		$scope.direction = Math.floor(feed.field7);
         $scope.speed = Math.floor(feed.field8);
         $scope.intermediaire =(17,27*$scope.temp)/(237,7+$scope.temp)+Math.log($scope.humidity/100);
-		
+
         $scope.dewpoint=parseFloat((237,7*$scope.intermediaire)/(17,27-$scope.intermediaire)).toFixed(1);
-		
+
 		 if($scope.speed >= 4.8) {
 			 $scope.tempercu = parseFloat(13.12 + 0.6215*$scope.temp - 11.37*Math.pow($scope.speed, 0.16) + 0.3965*$scope.temp*Math.pow($scope.speed, 0.16)).toFixed(1);
 		 }
 		 else {
 			 $scope.tempercu =parseFloat($scope.temp + 0.2*(0.1345*$scope.temp-1.59)*$scope.speed).toFixed(1);
 		 }
-		
+
 		//On veut faire la pluie sur la journée
-		$scope.pluie = parseFloat(feed.field4).toFixed(1);		
-                                                                             
+		$scope.pluie = parseFloat(feed.field4).toFixed(1);
+
     });
+		
     };
-	
-	
+
+
     $scope.control2 = function(){
 		$http.get('http://kgb.emn.fr:8001/channels/5/field/8.json?key=ZSAVTBI11WQOSJWY&results=100')
 	.then(function(result) {
@@ -46,21 +47,20 @@ pistApp.controller('AdvancedWeatherController', ['$scope', '$http', "$interval",
         		$scope.gust = element.field8;
         	}
         });
-	});     
+	});
     };
-    
-	
+
+
     $scope.control();
     $scope.control2();
-	
+
 	var intervalTempInt = $interval($scope.control, 10000);
 	var intervalReste= $interval($scope.control2, 10000);
 
 
-  $scope.$on('$destroy', function () { 
-    $interval.cancel(intervalTempInt); 
-    $interval.cancel(intervalReste); 
+  $scope.$on('$destroy', function () {
+    $interval.cancel(intervalTempInt);
+    $interval.cancel(intervalReste);
   });
-	
+
     }]);
-    
